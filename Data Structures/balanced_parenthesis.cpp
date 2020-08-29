0a8a3be6f0c6947011f9e1edf8e9bb5d1b6c6ad6
Sucheth <suchethgr@gmail.com>
2020-08-29 20:03:39 +0530

    balanced parentheses

---
+++ Data Structures/balanced_parenthesis.cpp
@@ -0,0 +1,49 @@
+#include<iostream>
+#include<string>
+using namespace std;
+int top;
+void check(string str,int n,string stack)
+{
+	for(int i=0;i<n;i++)
+	{
+		if(str[i]=='(')
+		{
+			top++;
+			stack[top]='(';	
+		}
+		else{
+			if(str[i]==')')
+			{
+				if(top==-1)
+				{
+					top--;
+				
+					break;
+				}
+				else
+				{
+					
+					top--;
+					
+				}
+			}
+		}
+	}
+	if(top==-1)
+	{
+		cout<<"String is balanced";
+	}
+	else
+	{
+		cout<<"String is unbalanced";
+	}
+}
+int main()
+{
+	top=-1;
+	string str;
+	string stack;
+	cin>>str;
+	check(str,str.size(),stack);
+	return 0; 
+}
